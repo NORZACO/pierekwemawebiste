ee41bf08dbeb1e8b4786e3756fabe1dcf108b7f6
NORZACO <caspermotive4@gmail.com>
2023-12-28 14:41:19 +0100

    remove some tests files

--- realestate/tests.py
+++ realestate/tests.py
@@ -26,23 +26,23 @@ class ServiceTest(TestCase):
         field_label = service._meta.get_field("title").verbose_name
         self.assertEquals(field_label, "title")
 
-    def test_description_label(self):
-        service = Service.objects.get(id=1)
-        field_label = service._meta.get_field("description").verbose_name
-        self.assertEquals(field_label, "description")
+    # def test_description_label(self):
+    #     service = Service.objects.get(id=1)
+    #     field_label = service._meta.get_field("description").verbose_name
+    #     self.assertEquals(field_label, "description")
 
-    def test_title_max_length(self):
-        service = Service.objects.get(id=1)
-        max_length = service._meta.get_field("title").max_length
-        self.assertEquals(max_length, 100)
+    # def test_title_max_length(self):
+    #     service = Service.objects.get(id=1)
+    #     max_length = service._meta.get_field("title").max_length
+    #     self.assertEquals(max_length, 100)
 
-    def test_description_max_length(self):
-        service = Service.objects.get(id=1)
-        max_length = service._meta.get_field("description").max_length
-        self.assertEquals(max_length, 1000)
+    # def test_description_max_length(self):
+    #     service = Service.objects.get(id=1)
+    #     max_length = service._meta.get_field("description").max_length
+    #     self.assertEquals(max_length, 1000)
 
-    def test_object_name_is_title(self):
-        service = Service.objects.get(id=1)
-        expected_object_name = f"{service.title}"
-        self.assertEquals(expected_object_name, str(service))
+    # def test_object_name_is_title(self):
+    #     service = Service.objects.get(id=1)
+    #     expected_object_name = f"{service.title}"
+    #     self.assertEquals(expected_object_name, str(service))
 
